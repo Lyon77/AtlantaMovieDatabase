a3e010546c52e9b884ff89aa5574c70afbae3f14
Lyon77 <rjl7799@gmail.com>
2019-11-28 15:19:21 -0500

    Created initial screen

--- MovieApplication.java
+++ MovieApplication.java
@@ -10,10 +10,16 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 
 import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TextField;
+
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+
+import javafx.scene.text.Text;
 
 import javafx.stage.Stage;
 
@@ -21,8 +27,6 @@ import javafx.stage.Stage;
 import java.util.Optional;
 
 public class MovieApplication extends Application {
-
-    private Label label;
 
     public static void main (String[] args) {
         try {
@@ -47,29 +51,89 @@ public class MovieApplication extends Application {
 
     @Override
     public void start(Stage stage) {
+        stage.setTitle("Movie Application");
 
+        loginScreen(stage);
+    }
+
+    private void loginScreen(Stage stage) {
         VBox root = new VBox();
         root.setPadding(new Insets(10));
         root.setSpacing(10);
 
-        this.label = new Label();
+        //Define the layout of the text fields and buttons
+        HBox h1 = new HBox();
+        Text usernameText = new Text("Username :  ");
+        TextField username = new TextField("Username");
+        h1.getChildren().addAll(usernameText, username);
 
-        Button button = new Button("Enter your name");
+        HBox h2 = new HBox();
+        Text passwordText = new Text("Password :  ");
+        TextField password = new TextField("Password");
+        h2.getChildren().addAll(passwordText, password);
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
+        HBox h3 = new HBox();
+        Button loginButton = new Button("Login");
+        Button registerButton = new Button("Register");
+        h3.getChildren().addAll(loginButton, registerButton);
 
+        //Set on Click actions
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(username.getText() + " " + password.getText());
+            }
+        });
+
+        registerButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                registerNavigation(stage);
+            }
+        });
+
+        //Finalize the stage
+        root.getChildren().addAll(h1, h2, h3);
+
+        Scene scene = new Scene(root, 450, 250);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void registerNavigation(Stage stage) {
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
+        root.setSpacing(10);
+
+        // Define the Buttons
+        Button userButton = new Button("User Only");
+        Button customerButton = new Button("Customer Only");
+        Button managerButton = new Button("Manager Only");
+        Button managerCustomerButton = new Button("Manager-Customer Only");
+        Button backButton = new Button("Back");
+
+        // Set on Click Buttons
+        userButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 showInputTextDialog();
             }
         });
 
-        root.getChildren().addAll(button, label);
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                loginScreen(stage);
+            }
+        });
+
+        // Finialize Stage
+        root.getChildren().addAll(userButton, customerButton, managerButton, managerCustomerButton, backButton);
 
         Scene scene = new Scene(root, 450, 250);
-        stage.setTitle("JavaFX TextInputDialog (o7planning.org)");
-        stage.setScene(scene);
 
+        stage.setScene(scene);
         stage.show();
     }
 
@@ -77,14 +141,14 @@ public class MovieApplication extends Application {
 
         TextInputDialog dialog = new TextInputDialog("Tran");
 
-        dialog.setTitle("o7planning");
+        dialog.setTitle("Movie Application");
         dialog.setHeaderText("Enter your name:");
         dialog.setContentText("Name:");
 
         Optional<String> result = dialog.showAndWait();
 
         result.ifPresent(name -> {
-            this.label.setText(name);
+            System.out.println(name);
         });
     }
 }
